d29348fca21835a07a1d50312d52f8989106f7f4
Rogerio Alves <gerasoa@gmail.com>
2018-06-20 15:41:33 -0300

    novo commit

---
+++ node-str/src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://gerasoa:123mudar@ds016068.mlab.com:16068/gerasoa_node_store',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=geranodestr;AccountKey=THPUcTrdNhYHbBtUNF+AuM7czDJgtW0T1h017tMNYYmKYmYbZ3MYxfTPAsYYVXUveewlI9wMyaDykhtqDUDNjQ==;EndpointSuffix=core.windows.net'
+}
